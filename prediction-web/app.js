bd19327634d82ed9be68674c2dda453c638d4121
tonihurtado <txni@gmx.com>
2021-11-18 16:05:27 +0100

    Prediction web development and Deployment configuration
    Signed-off-by: tonihurtado <txni@gmx.com>

--- prediction-web/app.js
+++ prediction-web/app.js
@@ -4,121 +4,125 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const URL_CB = process.env.URL_CB || "http://orion:1026/v2/entities";
-const PORT = process.env.PORT  ? (process.env.PORT) : 3000;
-const MONGO_URI = process.env.MONGO_URI || "mongodb://mongodb-svc:27017/tfm";
+const URL_CB = process.env.URL_CB || "http://tfm.test/orion" //"http://orion:1026/v2/entities";
+const PORT = process.env.PORT ? (process.env.PORT) : 3000;
+const MONGO_URI = process.env.MONGO_URI || "mongodb://tfm.test/orion/tfm";
 const fetch = require('cross-fetch')
-console.log("Orion URL: "+ URL_CB);
+console.log("Orion URL: " + URL_CB);
 
 const connectWithRetry = () => {
-	mongoose.connect(MONGO_URI).then(()=>{
-    console.log('MongoDB is connected to the web');
-  }).catch(err=>{ 
-    console.log('MongoDB connection with web unsuccessful, retry after 5 seconds.');
-    setTimeout(connectWithRetry, 5000);
-  })
+    mongoose.connect(MONGO_URI).then(() => {
+        console.log('MongoDB is connected to the web');
+    }).catch(err => {
+        console.log('MongoDB connection with web unsuccessful, retry after 5 seconds.');
+        setTimeout(connectWithRetry, 5000);
+    })
 }
 
-const find = (name, query, cb)  => {
-    mongoose.connection.db.collection(name, function (err, collection) {
-    	if (err) {
-    		cb(err);
-    	} else {
-    		collection.find(query).toArray(function(err, results){
-    			console.log(err)
-	       		cb(err,results);
-	       	});
-    	}
-   	});
+const find = (name, query, cb) => {
+    mongoose.connection.db.collection(name, function(err, collection) {
+        if (err) {
+            cb(err);
+        } else {
+            collection.find(query).toArray(function(err, results) {
+                console.log(err)
+                cb(err, results);
+            });
+        }
+    });
 }
 
 connectWithRetry()
 
 const createAttr = (attr) => {
-	return {"value": attr, "type": isNaN(attr) ? "String" : "Integer"};
+    return { "value": attr, "type": isNaN(attr) ? "String" : "Integer" };
 }
 
 
 
 const updateEntity = (data) => {
-	console.log(data);
-	fetch(URL_CB, {
-		body: JSON.stringify(data),
-		headers: { "Content-Type": "application/json" },
-		method: "PATCH"
-	})
-	.then(res=> {
-		console.log("Reply from Orion", res.ok)
-		if (res.ok) {
-			io.to(data.socketId.value).emit("messages",{type: "CONFIRMATION", payload:{ msg: "Your request is being processed"}});
-			return;
-		} 
-		throw new Error("Error")
-	})
-	.catch(e=>{
-		io.to(data.socketId.value).emit("messages",{type: "ERROR", payload:{ msg: "There has been a problem with your request"}});
-		console.error(e);
-	});
+    console.log(data);
+    fetch(URL_CB, {
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+            method: "PATCH"
+        })
+        .then(res => {
+            console.log("Reply from Orion", res.ok)
+            if (res.ok) {
+                io.to(data.socketId.value).emit("messages", { type: "CONFIRMATION", payload: { msg: "Your request is being processed" } });
+                return;
+            }
+            throw new Error("Error")
+        })
+        .catch(e => {
+            io.to(data.socketId.value).emit("messages", { type: "ERROR", payload: { msg: "There has been a problem with your request" } });
+            console.error(e);
+        });
 }
 
 server.listen(PORT, function() {
-	console.log("Listening on port " + PORT);
+    console.log("Listening on port " + PORT);
 });
 
 
 io.on('connection', function(socket) {
-	console.log('New socket connection');
-	socket.on('predict',(msg)=>{
-		const {name, year, month, day, weekday, time, predictionId } = msg;
-		updateEntity({ 
-			"name": createAttr(name),
-			"year": createAttr(year),
-			"month": createAttr(month),
-			"day": createAttr(day),
-			"weekday": createAttr(weekday),
-			"time": createAttr(time),
-			"predictionId": createAttr(predictionId),
-			"socketId": createAttr(socket.id)
-		});
-	})
+    console.log('New socket connection');
+    socket.on('predict', (msg) => {
+        const { name, year, month, day, weekday, time, predictionId } = msg;
+        updateEntity({
+            "name": createAttr(name),
+            "year": createAttr(year),
+            "month": createAttr(month),
+            "day": createAttr(day),
+            "weekday": createAttr(weekday),
+            "time": createAttr(time),
+            "predictionId": createAttr(predictionId),
+            "socketId": createAttr(socket.id)
+        });
+    })
 });
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public/"));
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 
-app.post("/notify",function(req,res){
-	if (req.body && req.body.data) {
-		req.body.data.map(({socketId, predictionId, predictionValue, name, weekday, time})=>{
-			io.to(socketId.value).emit('messages', {type: "PREDICTION", payload: {
-				socketId: socketId.value,
-				name: name.value,
-				weekday: weekday.value,
-				time: time.value,
-				predictionId: predictionId.value,
-				predictionValue: predictionValue.value
-			}});
-		});
-	}
-	res.sendStatus(200);
+app.post("/notify", function(req, res) {
+    if (req.body && req.body.data) {
+        req.body.data.map(({ socketId, predictionId, predictionValue, name, weekday, time }) => {
+            io.to(socketId.value).emit('messages', {
+                type: "PREDICTION",
+                payload: {
+                    socketId: socketId.value,
+                    name: name.value,
+                    weekday: weekday.value,
+                    time: time.value,
+                    predictionId: predictionId.value,
+                    predictionValue: predictionValue.value
+                }
+            });
+        });
+    }
+    res.sendStatus(200);
 });
 
-const fromEntries = arr => Object.assign({}, ...Array.from(arr, ([k, v]) => ({[k]: v}) ));
+const fromEntries = arr => Object.assign({}, ...Array.from(arr, ([k, v]) => ({
+    [k]: v })));
 
-app.get("/predictions",(req,res)=>{
-	find("sth_x002f", undefined, (err, predictions)=>{
-		if (err) {
-			res.sendStatus(500);
-		} else {
-			var obj =predictions.reverse().reduce((acc,el,i)=>{
-				return {...acc, [el.recvTime]: [...(acc[el.recvTime] || []),[el.attrName, el.attrValue]]}
-			},{})
+app.get("/predictions", (req, res) => {
+    find("sth_x002f", undefined, (err, predictions) => {
+        if (err) {
+            res.sendStatus(500);
+        } else {
+            var obj = predictions.reverse().reduce((acc, el, i) => {
+                return {...acc, [el.recvTime]: [...(acc[el.recvTime] || []), [el.attrName, el.attrValue]] }
+            }, {})
 
-			for (let i in obj) {
-				obj[i] = fromEntries(obj[i]);
-			}
-			res.json(obj);
-		}
-		
-	});
+            for (let i in obj) {
+                obj[i] = fromEntries(obj[i]);
+            }
+            res.json(obj);
+        }
+
+    });
 })
--- prediction-web/public/predict.js
+++ prediction-web/public/predict.js
@@ -1,118 +1,114 @@
-const {$} = window;
+const { $ } = window;
 let predictionId = Date.now();
-$(function () {
-	var socket = io.connect('/', { 'forceNew': true });
-	function renderSpinner(){
-		var html = `<img src="spinner.gif" className="spinner"/>`;
-		document.getElementById('messages').innerHTML = html;
+$(function() {
+    var socket = io.connect('/', { 'forceNew': true });
 
-	}
-	function renderAlert(data, type = "primary") {
-		var html =  `<div class="alert alert-dismissible alert-${type}">
-		  <button type="button" class="close" data-dismiss="alert">&times;</button>
+    function renderSpinner() {
+        var html = `<img src="spinner.gif" className="spinner"/>`;
+        document.getElementById('messages').innerHTML = html;
+
+    }
+
+    function renderAlert(data, type = "primary") {
+        var html = `<div <div class="notification is-danger is-light">
+		<button class="delete"></button>
 		  ${data}
-		</div>` ;
-		document.getElementById('messages').innerHTML = html;
-	}
+		</div>`;
+        document.getElementById('messages').innerHTML = html;
+    }
 
-	function renderResult(data) {
-		var html =  `<div id="result-container">
-			<div id="result">0</div>
-			<div id="Available spots">purchases</div>
-		</div>` ;
-		document.getElementById('messages').innerHTML = html;
-		let countdown = 0;
-		let interval = setInterval(()=>{
-			if (countdown < data){
-				countdown++;
-				document.getElementById('messages').innerHTML = `<div id="result-container">
-					<div id="result">${countdown}</div>
-					<div id="purchases">Available spots</div>
-				</div>`;
-			} else {
-				interval = null;
-			}
-		}, 10);
-		$('#result');
-	}
+    document.addEventListener('DOMContentLoaded', () => {
+        (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+            const $notification = $delete.parentNode;
 
-	socket.on('messages', function(action) {
-		try{
-			switch(action.type) {
-				case "CONFIRMATION":
-					renderSpinner();
-					break;
-				case "ERROR":
-					renderAlert(action.payload.msg, "danger");
-					break;
-				case "PREDICTION":
-				console.log(action.payload.predictionId,predictionId)
-					if (predictionId === (action.payload.predictionId)) {
-						renderResult(action.payload.predictionValue);
-					}
-					break;
-				default:
-					console.error("Unrecognized message type");
-			}
-		} catch (e) {
-			console.error(e)
-		}
-	});
-
-
-	const updateDate = (date) => {
-	    date.setHours(0,0,0,0)
-	    $("#date").val(date);
-	    $("#start").focus();
-	};
-	const getQueryStringValue = (key) => decodeURIComponent(window.location.search.replace(new RegExp(`^(?:.*[&\\?]${encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&")}(?:\\=([^&]*))?)?.*$`, "i"), "$1"));
-
-    $.datepicker.setDefaults({
-        "firstDay": 1,
-        "isRTL": false,
-        "showMonthAfterYear": false,
-        "yearSuffix": "",
-        "dateFormat": "dd/mm/yy",
+            $delete.addEventListener('click', () => {
+                $notification.parentNode.removeChild($notification);
+            });
+        });
     });
-    $.datepicker.setDefaults($.datepicker.regional.es);
-    $("#datepicker").datepicker({
-        "onSelect" () {
-            const date = $("#datepicker").datepicker("getDate");
-            updateDate(date);
-        },
-        "beforeShowDay" (date) {
-            if ($.inArray(date.getTime(), window.selectedDates) !== -1) {
-                return [
-                    true,
-                    "turn-day-highlight"
-                ];
+
+    function renderResult(data) {
+        var html = `<div id="result-container">
+			<div id="result">0</div>
+			<div id="Plazas libres">purchases</div>
+		</div>`;
+        document.getElementById('messages').innerHTML = html;
+        let countdown = 0;
+        let interval = setInterval(() => {
+            if (countdown < data) {
+                countdown++;
+                document.getElementById('messages').innerHTML = `<div id="result-container">
+					<div id="result">~${countdown}</div>
+					<div id="purchases">Plazas libres</div>
+				</div>`;
+            } else {
+                interval = null;
             }
-            return [
-                true,
-                "",
-                ""
-            ];
+        }, 10);
+        $('#result');
+    }
+
+    socket.on('messages', function(action) {
+        try {
+            switch (action.type) {
+                case "CONFIRMATION":
+                    renderSpinner();
+                    break;
+                case "ERROR":
+                    renderAlert(action.payload.msg, "danger");
+                    break;
+                case "PREDICTION":
+                    console.log(action.payload.predictionId, predictionId)
+                    if (predictionId === (action.payload.predictionId)) {
+                        renderResult(action.payload.predictionValue);
+                    }
+                    break;
+                default:
+                    console.error("Unrecognized message type");
+            }
+        } catch (e) {
+            console.error(e)
         }
     });
-    const myDate = new Date(getQueryStringValue("date"));
-    if (myDate && !isNaN(myDate.getDate())) {
-        $("#datepicker").datepicker("setDate", new Date(myDate));
+
+
+    const updateDate = (date) => {
+        $("#date").val(date);
+        $("#start").focus();
+    };
+    const getQueryStringValue = (key) => decodeURIComponent(window.location.search.replace(new RegExp(`^(?:.*[&\\?]${encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&")}(?:\\=([^&]*))?)?.*$`, "i"), "$1"));
+
+    const calendarOptions = {
+        "lang": "es-ES"
     }
-    const date = $("#datepicker").datepicker("getDate");
+    const calendars = bulmaCalendar.attach('[type="date"]', calendarOptions);
+
+    // Loop on each calendar initialized
+    calendars.forEach(calendar => {
+        // Add listener to select event
+        calendar.on('select', date => {
+            updateDate(date.data.value())
+            console.log(date.data.value());
+        });
+    });
+
+    var today = new Date();
+    var date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear()
     updateDate(date);
-    $("#newForm").on("submit", function () {
+
+    $("#newForm").on("submit", function() {
         const dateSubmitted = new Date($("#date").val());
         const time = parseInt($('#hourSelect').val());
-		const name =$('#nameSelect').val();
+        const name = $('#nameSelect').val();
         predictionId = "p-" + Date.now();
         const msg = {
-			name: name,
-        	year: dateSubmitted.getUTCFullYear(),
-        	month: dateSubmitted.getUTCMonth()+1,
-        	day: dateSubmitted.getUTCDate()+1,
-        	weekday: dateSubmitted.getDay(),
-        	time: time,
-        	predictionId: predictionId
+            name: name,
+            year: dateSubmitted.getUTCFullYear(),
+            month: dateSubmitted.getUTCMonth() + 1,
+            day: dateSubmitted.getUTCDate() + 1,
+            weekday: dateSubmitted.getDay(),
+            time: time,
+            predictionId: predictionId
         }
         socket.emit("predict", msg);
         return false;
